0dc5cbec6bafcb48100758465ff93ab8f372a8fe
opzpy123 <1441120973@qq.com>
2020-02-12 22:39:37 +0800

    发帖前端页面实现以及部分界面美化

---
+++ src/main/java/com/opzpy123/mypeojectdemo/controller/PublishController.java
@@ -0,0 +1,14 @@
+package com.opzpy123.mypeojectdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PublishController {
+
+    @GetMapping("/publish")
+    public String publish(){
+        return "publish";
+    }
+
+}
